4f484088222d6a2cc01adf87063c5785fd56aa94
Sumama Rahim <sumama.rahim@yandex.com>
2025-05-09 09:16:13 -0400

    Create if-else.java
    this problem demonstrates how to use conditions in java

---
+++ hr/if-else.java
@@ -0,0 +1,55 @@
+// Java If-Else problem
+// Dated: 09 May 2025
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N >= 1 && N <= 100)
+        {
+            int odd = N % 2;
+            if (odd == 0)
+            {
+                // working
+                if (N >= 2 && N <= 5)
+                {
+                    System.out.println("Not Weird");
+                }
+                
+                // working
+                else if (N >= 6 && N <= 20)
+                {
+                    System.out.println("Weird");
+                }
+                
+                // working
+                else // if (odd > 20)
+                {
+                    System.out.println("Not Weird");
+                }
+            }
+            
+            // working
+            else // if (odd == 0)
+            {
+                
+                
+                System.out.println("Weird");
+            }
+        }
+        scanner.close();
+    }
+}
