aca36e0e57a31998d42c5f9daac541260fd07052
Sumama Rahim <sumama.rahim@yandex.com>
2025-05-11 07:33:07 -0400

    Create Day1.java
    basic data types in java

---
+++ hr/30 days challange/Day1.java
@@ -0,0 +1,29 @@
+// Data 09 May 2025
+// Data types
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int mi;
+        double md;
+        String ms;
+        
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+        
+        Scanner sc = new Scanner(System.in);
+        mi = sc.nextInt();
+        md = sc.nextDouble();
+        sc.nextLine();
+        ms = sc.nextLine();
+        
+        System.out.println(i + mi);
+        System.out.println(d + md);
+        System.out.println(s + ms);
+    }
+}
